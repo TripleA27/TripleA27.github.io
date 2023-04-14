db62f6ac185d283514907295b4d2d853038bc2c6
TripleA27 <130704153+TripleA27@users.noreply.github.com>
2023-04-13 22:20:27 -0400

    Fixed the popup

--- PasswordAdder.js
+++ PasswordAdder.js
@@ -9,7 +9,7 @@ function addPassword() {
 
     if (weblink != undefined && webpass != undefined) {
         console.log("Now time to create a bookmark using the website " + weblink + " with the password " + webpass)
-        output = "javascript:(function() { var ask = prompt('Please enter your password'); if(ask == '" + webpass + "') { window.open('" + weblink + "') } else { alert('Wrong password!'); window.open('https://www.youtube.com/watch?v=Iovc1kB0VCY'); } } )();"
+        output = "javascript:(function() { var ask = prompt('Please enter your password'); if(ask == '" + webpass + "') { window.open('" + weblink + "') } else if(ask != undefined) { alert('Wrong password!'); window.open('https://www.youtube.com/embed/Iovc1kB0VCY?autoplay=1'); } } )();"
         document.getElementById("outp").value = output
         return true
     }
