3a88a42fff87a000fede5f3206555863146f7bc0
TripleA27 <130704153+TripleA27@users.noreply.github.com>
2023-04-13 20:19:59 -0400

    Spits out the ip if you get the password wrong.

--- PasswordAdder.js
+++ PasswordAdder.js
@@ -5,10 +5,14 @@ var output;
 function addPassword() {
     webpass = document.getElementById("pword").value
     weblink = document.getElementById("wlink").value
-    
+    var ip;
+
     if (weblink != undefined && webpass != undefined) {
         console.log("Now time to create a bookmark using the website " + weblink + " with the password " + webpass)
-        output = "javascript:(function() { var ask = prompt('Please enter your password'); if(ask == '" + webpass + "') { window.open('" + weblink + "') } } )();"
+        fetch('https://api.ipify.org/').then(
+            r => r.text(ip)
+        ).then();
+        output = "javascript:(function() { var ask = prompt('Please enter your password'); if(ask == '" + webpass + "') { window.open('" + weblink + "') } else { alert('Wrong password! '" + ip + "' ) } } )();"
         document.getElementById("outp").value = output
         return true
     }
